8fe603a766ad7ecc979890a45343fb5367d7b87d
David Basarab <dbasarab617@gmail.com>
2020-08-04 23:00:13 -0400

    playing with segment

--- segment.py
+++ segment.py
@@ -1,42 +1,44 @@
+#!/usr/bin/python
+
 import time
-import board
-import busio
-from adafruit_ht16k33 import segments
+import datetime
+from Adafruit_LED_Backpack import SevenSegment
 
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+# ===========================================================================
+# Clock Example
+# ===========================================================================
+segment = SevenSegment.SevenSegment(address=0x70)
 
-# Create the LED segment class.
-# This creates a 7 segment 4 character display:
-display = segments.Seg7x4(i2c)
+# Initialize the display. Must be called once before using the display.
+segment.begin()
 
-# Clear the display.
-display.fill(0)
+print("Press CTRL+C to exit")
 
-# Can just print a number
-display.print(42)
-time.sleep(1)
+# Continually update the time on a 4 char, 7-segment display
+try:
+    while True:
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute = now.minute
+        second = now.second
 
-# Set the first character to '1':
-display[0] = '1'
-# Set the second character to '2':
-display[1] = '2'
-# Set the third character to 'A':
-display[2] = 'A'
-# Set the forth character to 'B':
-display[3] = 'B'
-time.sleep(1)
+        segment.clear()
 
-numbers = [0.0, 1.0, -1.0, 0.55, -0.55, 10.23, -10.2, 100.5, -100.5]
+        # Set hours
+        segment.set_digit(0, "A")  # Tens
+        segment.set_digit(1, hour % 10)  # Ones
+        # Set minutes
+        segment.set_digit(2, int(minute / 10))  # Tens
+        segment.set_digit(3, minute % 10)  # Ones
+        # Toggle colon
+        segment.set_colon(second % 2)  # Toggle colon at 1Hz
 
-# print negative and positive floating point numbers
-for i in numbers:
-    display.print(i)
-    time.sleep(0.5)
+        # Write the display buffer to the hardware.  This must be called to
+        # update the actual display LEDs.
+        segment.write_display()
 
-# print hex values, enable colon
-for i in range(0xFF):
-    display.fill(0)
-    display.print(':')
-    display.print(hex(i))
-    time.sleep(0.25)
+        # Wait a quarter second (less than 1 second to prevent colon blinking getting$
+        time.sleep(0.25)
+except KeyboardInterrupt:
+    segment.clear()
+    segment.write_display()

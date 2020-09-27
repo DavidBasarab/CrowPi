e418a39974654ebcb9a8645029f0027e549ecbc2
David Basarab <dbasarab617@gmail.com>
2020-09-27 15:23:39 -0400

    working with input now

--- Learning.py
+++ Learning.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python
 
-from msvcrt import getch
-import RPI.GPIO as GPIO
+# import RPI.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
 ledPin = 4
-escapeKey = b'\x1b'
-spaceKey = b' '
-oneKey = b'1'
 pinOn = False
 
-GPIO.setup(ledPin, GPIO.OUT)
+# GPIO.setup(ledPin, GPIO.OUT)
+# GPIO.ouput(ledPin, 0)
 
-GPIO.ouput(ledPin, 0)
 
 while True:
-    key = getch()
+    key = input("Action, press q to quit: ")
 
     print(key)
 
-    if key == spaceKey:
+    if key == ' ':
         print("space pushed")
 
-    if key == oneKey:
+    if key == '1':
 
         if pinOn:
             print("turning led on")
-            GPIO.ouput(ledPin, 0)
-            pinOn = False
+            # GPIO.ouput(ledPin, 0)
+            # pinOn = False
         else:
             print("turning led off")
-            GPIO.ouput(ledPin, 1)
-            pinOn = True
+            # GPIO.ouput(ledPin, 1)
+            # pinOn = True
 
-    if key == escapeKey:
+    if key == 'q':
         print("escape key pressed exiting. . .")
         break
 

bba9df7fe6aa83dfe7e9023ff19747934d8539a3
David Basarab <dbasarab617@gmail.com>
2020-09-27 15:10:31 -0400

    using pins

--- Learning.py
+++ Learning.py
@@ -1,8 +1,19 @@
-from msvcrt import getch
+#!/usr/bin/python
 
+from msvcrt import getch
+import RPI.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+ledPin = 4
 escapeKey = b'\x1b'
 spaceKey = b' '
 oneKey = b'1'
+pinOn = False
+
+GPIO.setup(ledPin, GPIO.OUT)
+
+GPIO.ouput(ledPin, 0)
 
 while True:
     key = getch()
@@ -13,10 +24,18 @@ while True:
         print("space pushed")
 
     if key == oneKey:
-        print("one is pushed")
+
+        if pinOn:
+            print("turning led on")
+            GPIO.ouput(ledPin, 0)
+            pinOn = False
+        else:
+            print("turning led off")
+            GPIO.ouput(ledPin, 1)
+            pinOn = True
 
     if key == escapeKey:
-        print("escape key pressed")
+        print("escape key pressed exiting. . .")
         break
 
 
